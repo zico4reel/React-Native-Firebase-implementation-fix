26c738bd8cee16d7ab42e3f5f10b80e31d6d08eb
Ezekiel <zico4reel@gmail.com>
2019-07-09 12:15:13 +0100

    First Fix
    no much writeup here, just said I should help others.

---
+++ react-native.config.js
@@ -0,0 +1,14 @@
+﻿module.exports = {
+  dependencies: {
+    'react-native-firebase': {
+      platforms: {
+        android: {
+          "sourceDir": "./node_modules/react-native-firebase/android",
+          "folder": "./node_modules/react-native-firebase",
+          "packageImportPath": "import io.invertase.firebase.RNFirebasePackage;",
+          "packageInstance": "new RNFirebasePackage()"
+        }
+      }
+    }
+  }
+};
