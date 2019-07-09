daf8dad4b3739a8aef0f358f59091e346f4109e9
Ezekiel <zico4reel@gmail.com>
2019-07-09 13:36:13 +0100

    fix
    fix

--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,11 @@
-﻿module.exports = {
+module.exports = {
   dependencies: {
     'react-native-firebase': {
       platforms: {
+        ios: { podspecPath: path.join(__dirname, 'ios', 'RNFirebase.podspec') },
         android: {
-          "sourceDir": "./node_modules/react-native-firebase/android",
-          "folder": "./node_modules/react-native-firebase",
+          /*"sourceDir": "./node_modules/react-native-firebase/android",
+          "folder": "./node_modules/react-native-firebase",*/
           "packageImportPath": "import io.invertase.firebase.RNFirebasePackage;",
           "packageInstance": "new RNFirebasePackage()"
         }
