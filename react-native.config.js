076f45c0fb1451cc53ca11b36b9b33c475a52f8a
Ezekiel <zico4reel@gmail.com>
2019-07-27 16:26:17 +0100

    Reverting to strictly android support
    So if you're seeing this, I've decided to revert back to my old commit code which was made for Android support, but might work for iOS, if there's any other further fix I will update

--- react-native.config.js
+++ react-native.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  dependencies: {
-    'react-native-firebase': {
-      platforms: {
-        ios: { podspecPath: path.join(__dirname, 'ios', 'RNFirebase.podspec') },
-        android: {
-          /*"sourceDir": "./node_modules/react-native-firebase/android",
-          "folder": "./node_modules/react-native-firebase",*/
-          "packageImportPath": "import io.invertase.firebase.RNFirebasePackage;",
-          "packageInstance": "new RNFirebasePackage()"
-        }
-      }
-    }
-  }
-};
+module.exports = { dependencies: { 'react-native-firebase': { platforms: { android: { "sourceDir": "./node_modules/react-native-firebase/android", "folder": "./node_modules/react-native-firebase", "packageImportPath": "import io.invertase.firebase.RNFirebasePackage;", "packageInstance": "new RNFirebasePackage()" } } } }};
